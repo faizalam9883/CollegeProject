da6bd61bab678493cebd0f5704d2c9ff69831b30
Faiz Alam <faizalam9883@gmail.com>
2020-12-07 20:59:39 +0530

    first commit

---
+++ memoryCard/main.js
@@ -0,0 +1,64 @@
+const cards = document.querySelectorAll(".memory-card")
+const scoreEl = document.querySelector("#score")
+const triesEl = document.querySelector("#tries")
+let hasFlippedCard = false
+let lockBoard = false
+let firstCard, secondCard
+let score = 0
+
+function flipCard() {
+	if (lockBoard) return
+	if (this === firstCard) return
+
+	this.classList.add("flip")
+
+	if (!hasFlippedCard) {
+		hasFlippedCard = true
+		firstCard = this
+
+		return
+	}
+
+	secondCard = this
+	checkForMatch()
+}
+
+function checkForMatch() {
+	let isMatch = firstCard.dataset.framework === secondCard.dataset.framework
+
+	isMatch ? disableCards() : unflipCards()
+}
+
+function disableCards() {
+	firstCard.removeEventListener("click", flipCard)
+	secondCard.removeEventListener("click", flipCard)
+
+	score += 1
+	resetBoard()
+}
+
+function unflipCards() {
+	lockBoard = true
+
+	setTimeout(() => {
+		firstCard.classList.remove("flip")
+		secondCard.classList.remove("flip")
+
+		resetBoard()
+	}, 1000)
+}
+
+function resetBoard() {
+	scoreEl.textContent = score
+	;[hasFlippedCard, lockBoard] = [false, false]
+	;[firstCard, secondCard] = [null, null]
+}
+
+;(function shuffle() {
+	cards.forEach((card) => {
+		let randomPos = Math.floor(Math.random() * 12)
+		card.style.order = randomPos
+	})
+})()
+
+cards.forEach((card) => card.addEventListener("click", flipCard))
